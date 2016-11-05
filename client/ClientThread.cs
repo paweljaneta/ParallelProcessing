d9b952a283fba181db61cb0b6d540790ff0993b3
paweljaneta <paul.007@o2.pl>
2016-11-05 18:36:54 +0100

    added skeleton of client thread

---
+++ client/ClientThread.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace client
+{
+    class ClientThread
+    {
+        int id;
+        BinaryReader inputStream;
+        BinaryWriter outputStream;
+
+        TcpClient connection;
+
+        Thread thread;
+
+        ClientThread(string adress, int port)
+        {
+            try
+            {
+                connection = new TcpClient(adress, port);
+
+                inputStream = new BinaryReader(connection.GetStream());
+                outputStream = new BinaryWriter(connection.GetStream());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void run()
+        {
+            //load dll
+            //execute
+        }
+
+        public bool isThreadAlive()
+        {
+            return thread.IsAlive;
+        }
+
+    }
+}
--- client/clientMain.cs
+++ client/clientMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace client
 {
@@ -10,6 +11,42 @@ namespace client
     {
         static void Main(string[] args)
         {
+            List<Thread> workingThreads = new List<Thread>();
+            Thread cpuLoadThread;
+            int numberOfCPUcores = Environment.ProcessorCount;
+            double flops;
+
+            while(true) //infinite loop
+            {
+                //connect to server
+
+                //do tests
+
+                //get dll
+
+                //start threads
+                //wait until finished
+                while(!threadsFinished(workingThreads))
+                {
+                    Thread.Sleep(1);
+                }
+
+            }
+
         }
+
+        public static bool threadsFinished(List<Thread> threads)
+        {
+            bool result = true;
+
+            foreach(Thread thread in threads)
+            {
+                if (thread.IsAlive)
+                    result = false;
+            }
+
+            return result;
+        }
+
     }
 }
