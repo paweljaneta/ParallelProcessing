27f6e0bc27eac9a1f705b76411f076256c7d83a4
paweljaneta <paul.007@o2.pl>
2016-11-12 18:35:34 +0100

    added messages to transfer types

--- communicationLibrary/Messages.cs
+++ communicationLibrary/Messages.cs
@@ -17,5 +17,58 @@ namespace communicationLibrary
         public const string connectionSpeedMeasurment = "conSpeedMeas";
         public const string flops = "FLOPS";
 
+        #region basicTypes
+        public const string byteTransfer = "byteTransfer";
+        public const string sbyteTransfer = "sbyteTransfer";
+        public const string shortTransfer = "shortTransfer";
+        public const string ushortTransfer = "ushortTransfer";
+        public const string intTransfer = "intTransfer";
+        public const string uintTransfer = "uintTransfer";
+        public const string longTransfer = "longTransfer";
+        public const string ulongTransfer = "ulongTransfer";
+        public const string floatTransfer = "floatTransfer";
+        public const string doubleTransfer = "doubleTransfer";
+        public const string decimalTransfer = "decimalTransfer";
+        public const string charTransfer = "charTransfer";
+        public const string stringTransfer = "stringTransfer";
+        public const string boolTransfer = "boolTransfer";
+        public const string objectTransfer = "objectTransfer";
+        #endregion
+
+        #region arrayTypes
+        public const string byteArrayTransfer = "byteArrayTransfer";
+        public const string sbyteArrayTransfer = "sbyteArrayTransfer";
+        public const string shortArrayTransfer = "shortArrayTransfer";
+        public const string ushortArrayTransfer = "ushortArrayTransfer";
+        public const string intArrayTransfer = "intArrayTransfer";
+        public const string uintArrayTransfer = "uintArrayTransfer";
+        public const string longArrayTransfer = "longArrayTransfer";
+        public const string ulongArrayTransfer = "ulongArrayTransfer";
+        public const string floatArrayTransfer = "floatArrayTransfer";
+        public const string doubleArrayTransfer = "doubleArrayTransfer";
+        public const string decimalArrayTransfer = "decimalArrayTransfer";
+        public const string charArrayTransfer = "charArrayTransfer";
+        public const string stringArrayTransfer = "stringArrayTransfer";
+        public const string boolArrayTransfer = "boolArrayTransfer";
+        public const string objectArrayTransfer = "objectArrayTransfer";
+        #endregion
+
+        #region listTypes
+        public const string byteListTransfer = "byteListTransfer";
+        public const string sbyteListTransfer = "sbyteListTransfer";
+        public const string shortListTransfer = "shortListTransfer";
+        public const string ushortListTransfer = "ushortListTransfer";
+        public const string intListTransfer = "intListTransfer";
+        public const string uintListTransfer = "uintListTransfer";
+        public const string longListTransfer = "longListTransfer";
+        public const string ulongListTransfer = "ulongListTransfer";
+        public const string floatListTransfer = "floatListTransfer";
+        public const string doubleListTransfer = "doubleListTransfer";
+        public const string decimalListTransfer = "decimalListTransfer";
+        public const string charListTransfer = "charListTransfer";
+        public const string stringListTransfer = "stringListTransfer";
+        public const string boolListTransfer = "boolListTransfer";
+        public const string objectListTransfer = "objectListTransfer";
+        #endregion
     }
 }
